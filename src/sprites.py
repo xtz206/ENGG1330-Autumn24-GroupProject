4a4b3f6f93a22806e2cc023a8e002f018a734a4d
xtz206 <ywx2006@outlook.com>
2024-10-30 22:01:19 +0800

    feat: update spirites.Maze

--- src/sprites.py
+++ src/sprites.py
@@ -10,12 +10,16 @@ class Sprite:
 
 
 class Maze(Sprite):
-    def __init__(self, win, height, width, blocks, start):
+    def __init__(self, win, height, width, blocks, start, end):
         super().__init__(win, height, width, blocks)
         self.start = start
+        self.end = end
 
     def get_start(self):
         return self.start
+    
+    def get_end(self):
+        return self.end
 
     def check_solid(self, y, x):
         index = y * self.width + x
