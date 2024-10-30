5beeea70a7640455d1f9e267898b8d2489f10ef6
xtz206 <ywx2006@outlook.com>
2024-10-30 20:36:25 +0800

    feat: create a json formatter for assets

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,14 @@
 def log_to_file(msg):
     with open("./logs.txt", 'a') as f:
         f.write(msg + '\n')
+
+def json_format(path):
+    import json
+
+    with open(path, 'r') as f:
+        data = json.load(f)
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+if __name__ == "__main__":
+    json_format("assets/mazes.json")
