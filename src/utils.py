2a024dbcdd45a881e2f7f3bbb4b0b5609a7d08e5
xtz206 <ywx2006@outlook.com>
2024-10-31 23:11:02 +0800

    feat: create maze checker

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import json
+
 def log_to_file(*msgs, sep=" ", end="\n"):
     with open("./logs.txt", 'a') as f:
         f.write(sep.join([format(msg) for msg in msgs]) + end)
@@ -5,10 +7,52 @@ def log_to_file(*msgs, sep=" ", end="\n"):
 def json_format(path):
     import json
 
+    
     with open(path, 'r') as f:
         data = json.load(f)
-    with open(path, 'w') as f:
-        json.dump(data, f, indent=4)
+    if data is None:
+        return
+    else:
+        with open(path, 'w') as f:
+            json.dump(data, f, indent=4)
+
+def check_maze(maze):
+    height = maze["height"]
+    width = maze["width"]
+    start = maze["start"]
+    end = maze["end"]
+    routes = maze.get("routes", {})
+    blocks = maze["block_names"]
+
+    if len(blocks) != height * width:
+        return f"Block Count is not consistent", 0
+
+    index = start[0] * width + start[1]
+    if blocks[index] != "start":
+        return f"Start is not consistent", index
+    
+    index = end[0] * width + end[1]
+    if blocks[index] != "end":
+        return f"End is not consistent", index
+    
+    for route in routes.values():
+        for block in route:
+            index = block[0] * width + block[1]
+            if blocks[index] != "air":
+                return f"route {block} is blocked", index
+    
+    return None, None
+
+
+def check_mazes(path):
+    with open(path, 'r') as f:
+        data = json.load(f)
+    for index, maze in enumerate(data):
+        reason, index = check_maze(maze)
+        if reason is not None:
+            print(f"Maze {index} has problems, Reason: {reason}, Index: {index}")
+
 
 if __name__ == "__main__":
-    json_format("assets/mazes.json")
+    # json_format("assets/mazes.json")
+    check_mazes("assets/mazes.json")
